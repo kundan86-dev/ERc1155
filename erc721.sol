3e58a3532ebfb972fe8d23ed79aee6b8074f9274
Kundan-sharma <kundan.sharma@indicchain.com>
2023-09-07 17:14:22 +0530

    Erc1155 deployed

---
+++ erc721.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFTToken is ERC721URIStorage, Ownable {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("goku","go"){
+        
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns(uint){
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
